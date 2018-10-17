074c308581531cae519660eb705ac7df2bb88bc0
ssatm <44239678+ssatm@users.noreply.github.com>
2018-10-17 20:55:42 +0300

    Update st1.js

--- st1.js
+++ st1.js
@@ -1,6 +1,12 @@
-const Discord = require ('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+const request = require('request');
+const fs = require('fs');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
 const prefix = "!"
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
